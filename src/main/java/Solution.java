1a2ac55dc703b3fa5e4a8b4b1422fdff58c6c659
2kfarid3 <fkarimli047@gmail.com>
2025-12-07 20:25:52 +0900

    If you remove duplicates on sorted array, check if i != i-1 and start from i = 1, because i = 0 always will be unique

---
+++ src/main/java/Solution.java
@@ -0,0 +1,24 @@
+public class Solution {
+    public static void main(String[] args) {
+        int[] nums = new int[]{0,0,1,1,1,2,2,3,3,4};
+        int k = removeDuplicates(nums);
+        System.out.println(k);
+    }
+
+
+
+
+    public static int removeDuplicates(int[] nums) {
+
+        int k = 1;
+
+        for (int i = 1; i < nums.length; i++) {
+            if (nums[i] != nums[i - 1]) {
+                nums[k] = nums[i];
+                k++;
+            }
+        }
+
+        return k;
+    }
+}
