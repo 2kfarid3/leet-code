55f6d5ef6a6ed11f0a1381644b0ccf2ef122342a
2kfarid3 <fkarimli047@gmail.com>
2025-12-14 22:48:46 +0900

    Start from the last index and increase if <9, if all 9, make all 0

---
+++ src/main/java/PlusOne.java
@@ -0,0 +1,27 @@
+import java.util.Arrays;
+
+public class PlusOne {
+    public static void main(String[] args) {
+        int[] digits = new int[]{9,8,7,6,5,4,3,2,1,0};
+        System.out.println(Arrays.toString(plusOne(digits)));
+    }
+
+
+
+
+
+    public static int[] plusOne(int[] digits) {
+
+        for (int i = digits.length - 1; i >= 0; i--) {
+            if (digits[i] < 9) {
+                digits[i]++;
+                return digits;
+            }
+            digits[i] = 0;
+        }
+
+        int[] res = new int[digits.length + 1];
+        res[0] = 1;
+        return res;
+    }
+}
